cc81906f732511b1ba7e001ac4833e53484ce5c6
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 14:05:54 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -9,9 +9,10 @@ data "terraform_remote_state" "vm" {
   }
 }
 
-provisioner "remote-exec" {
-    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
-
+resource "null_resource" "one" {
+  provisioner "remote-exec" {
+    inline = "sudo apt update"
+    
     connection {
       host        = data.terraform_remote_state.vm.outputs.public_ip
       type        = "ssh"
@@ -19,3 +20,20 @@ provisioner "remote-exec" {
       password    = var.user_password
     }
   }
+}
+
+resource "null_resource" "two" {
+  provisioner "remote_exec" {
+    inline = "sudo apt install python3 -y"
+    
+    connection {
+      host        = data.terraform_remote_state.vm.outputs.public_ip
+      type        = "ssh"
+      user        = var.user_name
+      password    = var.user_password
+    }
+  depends on = [
+    null_resource.one
+  ]
+  }
+}
