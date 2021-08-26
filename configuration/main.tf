d2bba45e91d5ddf9195a4f1c8c42370a9d06fe8f
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 14:26:21 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -21,7 +21,7 @@ resource "null_resource" "raddit" {
 
     connection {
       type        = "ssh"
-      host        = data.terraform_remote_state.vm.public_ip
+      host        = data.terraform_remote_state.vm.outputs.public_ip
       user        = var.user_name
       password    = var.user_password
       timeout     = "1m"
