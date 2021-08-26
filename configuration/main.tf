962263bf1af704b0a1a74c8c44f9352dd1565f6f
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 14:16:18 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -9,31 +9,46 @@ data "terraform_remote_state" "vm" {
   }
 }
 
-resource "null_resource" "one" {
-  provisioner "remote-exec" {
-    inline = "sudo apt update"
-    
-    connection {
-      host        = data.terraform_remote_state.vm.outputs.public_ip
-      type        = "ssh"
-      user        = var.user_name
-      password    = var.user_password
-    }
-  }
-}
+# resource "null_resource" "one" {
+#  provisioner "remote-exec" {
+#    inline = "sudo apt update"
+#    
+#    connection {
+#      host        = data.terraform_remote_state.vm.outputs.public_ip
+#      type        = "ssh"
+#      user        = var.user_name
+#      password    = var.user_password
+#    }
+#  }
+# }
 
-resource "null_resource" "two" {
-  provisioner "remote_exec" {
-    inline = "sudo apt install python3 -y"
+# resource "null_resource" "two" {
+#  provisioner "remote_exec" {
+#    inline = "sudo apt install python3 -y"
+#    
+#    connection {
+#      host        = data.terraform_remote_state.vm.outputs.public_ip
+#      type        = "ssh"
+#      user        = var.user_name
+#      password    = var.user_password
+#    }
+#    depends_on = [
+#      null_resource.one
+#    ]
+#  }
+# }
+
+resource "null_resource" "load_file {
+  
+  provisioner "file" {
+    source = "deploy.sh"
+    destination = "/home/raddit-user/deploy.sh"
     
     connection {
-      host        = data.terraform_remote_state.vm.outputs.public_ip
-      type        = "ssh"
-      user        = var.user_name
-      password    = var.user_password
+      host    = data.terraform_remote_state.vm.outputs.public_ip
+      type    = "ssh"
+      user    = var.user_name
+      password = var.user_password
     }
-    depends_on = [
-      null_resource.one
-    ]
   }
 }
