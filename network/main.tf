5a034624e901b2a8aea7782f2a0a2e151ea385e6
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 12:59:01 -0500

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 # Create virtual network
 resource "azurerm_virtual_network" "hashinet" {
   name                = "vpVnet"
