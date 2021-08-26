ad7329d6a9136062a5694bd07d7674d5842fe1b0
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 14:21:59 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -9,46 +9,22 @@ data "terraform_remote_state" "vm" {
   }
 }
 
-# resource "null_resource" "one" {
-#  provisioner "remote-exec" {
-#    inline = "sudo apt update"
-#    
-#    connection {
-#      host        = data.terraform_remote_state.vm.outputs.public_ip
-#      type        = "ssh"
-#      user        = var.user_name
-#      password    = var.user_password
-#    }
-#  }
-# }
+resource "null_resource" "raddit" {
+  provisioner "remote-exec" {
+    inline = [
+      "git clone https://github.com/Artemmkin/raddit.git",
+      "cd ./raddit",
+      "sudo bundle install",
+      "sudo systemctl start raddit",
+      "sudo systemctl enable raddit"
+    ]
 
-# resource "null_resource" "two" {
-#  provisioner "remote_exec" {
-#    inline = "sudo apt install python3 -y"
-#    
-#    connection {
-#      host        = data.terraform_remote_state.vm.outputs.public_ip
-#      type        = "ssh"
-#      user        = var.user_name
-#      password    = var.user_password
-#    }
-#    depends_on = [
-#      null_resource.one
-#    ]
-#  }
-# }
-
-resource "null_resource" "load_file" {
-  
-  provisioner "file" {
-    source = "deploy.sh"
-    destination = "/home/raddit-user/deploy.sh"
-    
     connection {
-      host    = data.terraform_remote_state.vm.outputs.public_ip
-      type    = "ssh"
-      user    = var.user_name
-      password = var.user_password
+      type        = "ssh"
+      host        = data.terraform_remote_state.vm.public_ip
+      user        = var.user name
+      password    = var.user_password
+      timeout     = "1m"
     }
   }
 }
