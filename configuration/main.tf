0766d74e6cf8759c3e93764b605004048cbe35ba
Viet Pham <37876479+vietpham123@users.noreply.github.com>
2021-08-26 13:50:30 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -1,1 +1,21 @@
+data "terraform_remote_state" "vm" {
+  backend = "remote"
+  
+  config = {
+    organization = "Hashi-Demo"
+    workspaces = {
+      name = "Raddit-VM"
+    }
+  }
+}
 
+provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
+
+    connection {
+      host        = data.terraform_remote_state.vm.outputs.public_ip
+      type        = "ssh"
+      user        = var.user_name
+      password    = var.user_password
+    }
+  }
